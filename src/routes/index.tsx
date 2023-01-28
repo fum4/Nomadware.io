8ec34391fb54da522ca415570bce7789d49955b4
Isis☕Caffe <52106389+fum4@users.noreply.github.com>
2023-01-28 17:45:55 +0200

    Fix warning & errors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import type { DocumentHead, RequestHandler } from '@builder.io/qwik-city';
+import type { DocumentHead } from '@builder.io/qwik-city';
 
 import Rain from '~/components/rain';
 
